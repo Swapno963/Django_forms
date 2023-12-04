bef14c22b0ea46e82347ef0cc07477a3d219435e
swapno963 <swapno963@gmail.com>
2023-12-05 00:58:38 +0600

    File upload can be done and it will be store at upload folder

--- first_app/forms.py
+++ first_app/forms.py
@@ -2,17 +2,18 @@ from django import forms
 
 class contactForm(forms.Form):
     name = forms.CharField(label='User Name')
-    email = forms.EmailField(label='User Eamil')
-    age = forms.IntegerField()
-    weight = forms.FloatField()
-    balance = forms.DecimalField()
-    check = forms.BooleanField()
-    birthday = forms.DateField()
-    # appointment = forms.DateRangeField()
-    appointment2 = forms.DateTimeField()
+    file =forms.FileField()
+    # email = forms.EmailField(label='User Eamil')
+    # age = forms.IntegerField()
+    # weight = forms.FloatField()
+    # balance = forms.DecimalField()
+    # check = forms.BooleanField()
+    # birthday = forms.DateField()
+    # # appointment = forms.DateRangeField()
+    # appointment2 = forms.DateTimeField()
 
-    CHOICES = [('S','Small'),('M','Medium'),('L','Large')]
-    size = forms.ChoiceField(choices = CHOICES)
+    # CHOICES = [('S','Small'),('M','Medium'),('L','Large')]
+    # size = forms.ChoiceField(choices = CHOICES)
 
-    MEAL = [('P','Pepperoni'),('M','Mashroom'),('C','Chicken')]
-    pizza = forms.MultipleChoiceField(choices=MEAL)
+    # MEAL = [('P','Pepperoni'),('M','Mashroom'),('C','Chicken')]
+    # pizza = forms.MultipleChoiceField(choices=MEAL)
--- first_app/views.py
+++ first_app/views.py
@@ -18,7 +18,18 @@ def submit_form(request):
 
 
 def DjangoForm(request):
-    form = contactForm(request.POST)
-    if form.is_valid():
-        print(form.cleaned_data)
+    if request.method == 'POST':
+        form = contactForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            with open('./first_app/upload/' + file.name, 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+            print(form.cleaned_data)
+            return render(request, 'django_form.html',{'form':form})
+    else:
+        form = contactForm()
     return render(request, 'django_form.html',{'form':form})
+
+
+
