5af31b925823c8464d7a63c12cb27d63d59be1da
swapno963 <swapno963@gmail.com>
2023-12-04 17:34:31 +0600

    Now We can take inpur from from.html and show result of from in about.html

--- first_app/views.py
+++ first_app/views.py
@@ -5,13 +5,12 @@ def home(request):
     return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
-
-def submit_form(request):
-    print(request.POST)
     if request.method == 'POST':
         name = request.POST.get('username')
         email = request.POST.get('useremail')
-        return render(request, 'form.html', {'name':name,'email':email})
+        return render(request, 'about.html', {'name':name,'email':email})
     else:
-        return render(request, 'form.html')
+        return render(request, 'about.html')
+
+def submit_form(request):
+    return render(request, 'form.html')
