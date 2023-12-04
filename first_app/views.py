d83aff118006125fe1ccaffac00a2c376de50435
swapno963 <swapno963@gmail.com>
2023-12-04 17:55:05 +0600

    added some style to from and now itcan take rating by select

--- first_app/views.py
+++ first_app/views.py
@@ -8,7 +8,8 @@ def about(request):
     if request.method == 'POST':
         name = request.POST.get('username')
         email = request.POST.get('useremail')
-        return render(request, 'about.html', {'name':name,'email':email})
+        select = request.POST.get('select')
+        return render(request, 'about.html', {'name':name,'email':email,'select':select})
     else:
         return render(request, 'about.html')
 
