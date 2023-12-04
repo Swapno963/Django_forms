58d68c7acca7a4805a1b0c4e3ee7072823a5c5fb
swapno963 <swapno963@gmail.com>
2023-12-04 18:55:41 +0600

    django form added

--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from . forms import contactForm
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -15,3 +15,10 @@ def about(request):
 
 def submit_form(request):
     return render(request, 'form.html')
+
+
+def DjangoForm(request):
+    form = contactForm(request.POST)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, 'django_form.html',{'form':form})
