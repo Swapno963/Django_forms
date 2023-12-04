e9ce35b1b2933f8f4fc9a7e7318192f5085a212b
swapno963 <swapno963@gmail.com>
2023-12-04 19:25:17 +0600

    some built in form field

--- first_app/forms.py
+++ first_app/forms.py
@@ -3,3 +3,10 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(label='User Name')
     email = forms.EmailField(label='User Eamil')
+    age = forms.IntegerField()
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    check = forms.BooleanField()
+    birthday = forms.DateField()
+    # appointment = forms.DateRangeField()
+    appointment2 = forms.DateTimeField()
